b24483d7ae840ec25aecea2f0661b0d78091d8e6
ainhoa <aaracarc@g.educaand.es>
2023-10-06 12:49:55 +0200

    Avance

--- src/main/java/org/ejercicios/Empresa.java
+++ src/main/java/org/ejercicios/Empresa.java
@@ -1,4 +1,17 @@
 package org.ejercicios;
 
+import Anotations.EmpleadoI;
+
 public class Empresa {
+    @EmpleadoI(
+            nombre = "Amancio",
+            apellidos = "Ortega",
+            dni = "66554433F",
+            direccion = "AV.DIPUTACION S/N, P.I. SABON 15142, ARTEIXO, LA CORUÑA",
+            telefono = "981185596",
+            clase = "Directivo",
+            codigoDespacho = "1"
+    )
+
+
 }
